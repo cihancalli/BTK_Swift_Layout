71926ffc8cc4bd4616f04b51b13a77248ad503f1
Cihan Çallı <cihan.callii@gmail.com>
2024-04-02 10:26:03 +0300

    first commit

--- BTKLayout/ViewController.swift
+++ BTKLayout/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        //Label
+        let myLabel = UILabel()
+        myLabel.text = "Test Label"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 30, width: width * 0.8, height: 60)
+        view.addSubview(myLabel)
+        
+        //Button
+        let myButton  = UIButton()
+        myButton.setTitle("Benim Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.65 - 50 , width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.benimFoksiyonum), for: UIControl.Event.touchUpInside)
+        
     }
-
-
+    
+    
+    @objc func benimFoksiyonum() {
+        print("kullanıcı button'a tıkladı")
+    }
+    
 }
 
